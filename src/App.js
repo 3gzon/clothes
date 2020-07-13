8a4d47c6750d7104ce9e9aea03e91f54ec01cfce
Egzon <egzon@starlabspro.com>
2020-07-13 21:21:20 +0200

    feat: added files for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <HomePage />
     </div>
   );
 }
---
+++ src/HomePage.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./homePage.scss"
+export default function HomePage() {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">JACKETS</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">WOMEN</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">MAN</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
