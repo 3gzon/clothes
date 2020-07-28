ee5839beb6357cbf82ac176e48af01bb84574b74
Egzon <egzon@starlabspro.com>
2020-07-28 22:03:06 +0200

    feat: added user redirect and user action type

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 import { connect } from 'react-redux'
 import HomePage from './pages/homepage/HomePage';
 import Shop from './pages/shop/shop'
@@ -38,16 +38,20 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={Shop} />
-          <Route path="/signin" component={SignInSignUp} />
+          <Route exact path="/signin"
+            render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInSignUp />)} />
         </Switch>
       </div>
     );
   }
 
 }
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
