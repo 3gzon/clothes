1c869e7b1b2cb09594c335d56564c83bb065f2c6
Egzon <egzon@starlabspro.com>
2020-08-01 23:03:44 +0200

    feat: selectors on react, reduce

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -3,13 +3,17 @@ import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './cart-icon.scss'
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 )
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-export default connect(null, mapDispatchToProps)(CartIcon)
+//selector
+const mapStateProps = ({ cart: { cartItems } }) => ({
+    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+})
+export default connect(mapStateProps, mapDispatchToProps)(CartIcon)
