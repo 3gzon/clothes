c6ceee3406a6cde745ba48b33bccc0caa911ca07
Egzon <egzon@starlabspro.com>
2020-08-02 14:11:58 +0200

    feat: checkout item

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import CheckoutItem from '../../components/checkout-item/checkout-item'
 import { createStructuredSelector } from 'reselect'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 import './checkout.scss'
@@ -24,7 +25,9 @@ const CheckoutPage = ({ cartItems, total }) => (
             </div>
         </div>
         {
-            cartItems.map(cartItem => cartItem.name)
+            cartItems.map(cartItem =>
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            )
         }
         <div className="total">
             <span>TOTAL:${total}</span>
