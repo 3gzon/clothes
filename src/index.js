487320710c777bfc922fb23a44bb7ffacd619011
Egzon <egzon@starlabspro.com>
2020-08-02 17:23:15 +0200

    feat: add to localstorage items, when we reload the page items doesn't remove, and I added redux persist library to manage localstorage

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import store from './redux/store'
+import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>
   ,
