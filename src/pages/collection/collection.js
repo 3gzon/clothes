89316c0cf1647337d3474a9b6bb8acdc5d80c379
Egzon <egzon@starlabspro.com>
2020-08-06 22:21:35 +0200

    feat: Nested Routing in Shop Page

---
+++ src/pages/collection/collection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CollectionItem from '../../components/collection-item/collection-item'
+import './collection.scss'
+
+const CollectionPage = ({ match }) => (
+    <div className="collection">
+        <h1>Collection</h1>
+    </div>
+)
+
+export default CollectionPage
+
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { Route } from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/collections-overview'
-
-const Shop = ({ collections }) => (
+import CollectionPage from '../collection/collection'
+const Shop = ({ match }) => (
     <div className="shop-page">
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
 export default Shop
