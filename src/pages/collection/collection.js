f759643d6b0fec645498ac603f78355e389ed2a7
Egzon <egzon@starlabspro.com>
2020-08-06 22:34:20 +0200

    feat:Collection Routing and Selector, changed dynamically routing of shop/:{path}

--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { selectCollection } from '../../redux/shop/shop.selectors'
 import CollectionItem from '../../components/collection-item/collection-item'
 import './collection.scss'
 
-const CollectionPage = ({ match }) => (
-    <div className="collection">
-        <h1>Collection</h1>
-    </div>
-)
+const CollectionPage = ({ collection }) => {
+    console.log(collection)
+    return (
+        <div className="collection">
+            <h1>Collection</h1>
+        </div>
+    )
+}
 
-export default CollectionPage
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
 
