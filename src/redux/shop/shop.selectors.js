adca69e563c3d7a77c23f04bc190f58d7c03c209
Egzon <egzon@starlabspro.com>
2020-08-11 22:00:23 +0200

    feat: data normalization, using objects instead array data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,11 +1,5 @@
 import { createSelector } from 'reselect'
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackets: 3,
-    womens: 4,
-    mens: 5
-}
+
 const selectShop = state => state.shop
 
 export const selectCollections = createSelector(
@@ -15,5 +9,5 @@ export const selectCollections = createSelector(
 
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
-    collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    collections => collections[collectionUrlParam]
 )
